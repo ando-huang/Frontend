1419b0173a35c724577512e1c663a15725b5e4ef
ando-huang <ahuang09182000@gmail.com>
2021-01-28 00:21:29 -0500

    initial commit

---
+++ basicsHTML/javascript/main.js
@@ -0,0 +1,11 @@
+//in the html, include this script at the bottom of the body\
+/* multi line comments, just like java*/
+
+var alert_message = "this is the message saved in a var";
+const console_message = "this is a constant message";
+var curr_age = 20;
+var exact_age = 20.4;
+
+alert(alert_message);
+console.log(console_message);
+console.log("My year next year will be " + (curr_age+1));
